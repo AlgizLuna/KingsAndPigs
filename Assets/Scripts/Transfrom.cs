145547688320402cd3a086067e03cb74065b0faa
Diana Gabriela Rios Anguiano <gabriela.algiz.98@gmail.com>
2025-09-29 12:29:04 -0600

    Estructura de GatherInput, PlayerController y movimientos 1D Axis

---
+++ Assets/Scripts/Transfrom.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+internal class Transfrom
+{
+    public static implicit operator Transfrom(Transform v)
+    {
+        throw new NotImplementedException();
+    }
+}
